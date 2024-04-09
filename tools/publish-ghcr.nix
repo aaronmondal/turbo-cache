a4b06b202e7c465538f2d390662193cf315c71ac
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2024-04-09 23:59:19 +0200

    Fix semver image workflow

--- tools/publish-ghcr.nix
+++ tools/publish-ghcr.nix
@@ -8,13 +8,22 @@ pkgs.writeShellScriptBin "publish-ghcr" ''
     --password-stdin \
     ghcr.io
 
+  IMAGE_NAME=$(nix eval .#$1.imageName --raw)
+
   # Commit hashes would not be a good choice here as they are not
   # fully dependent on the inputs to the image. For instance, amending
   # nothing would still lead to a new hash. Instead we use the
   # derivation hash as the tag so that the tag is reused if the image
   # didn't change.
-  IMAGE_TAG=$(nix eval .#$1.imageTag --raw)
-  IMAGE_NAME=$(nix eval .#$1.imageName --raw)
+  #
+  # If this script is invoked with a second argument the tag is overridden to
+  # use it as the tag instead. This is used in the release workflow.
+  if [[ "$2" ]] then
+    IMAGE_TAG=$2
+  else
+    IMAGE_TAG=$(nix eval .#$1.imageTag --raw)
+  fi
+
 
   TAGGED_IMAGE=''${GHCR_REGISTRY,,}/''${IMAGE_NAME}:''${IMAGE_TAG}
 
