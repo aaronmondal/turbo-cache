64984c1e761d6888c10068e799c633dade9d9a9b
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2024-12-12 18:32:00 +0100

    Bump siso image

--- flake.nix
+++ flake.nix
@@ -272,8 +272,8 @@
           name = "siso-chromium";
           fromImage = pullImage {
             imageName = "gcr.io/chops-public-images-prod/rbe/siso-chromium/linux";
-            imageDigest = "sha256:26de99218a1a8b527d4840490bcbf1690ee0b55c84316300b60776e6b3a03fe1";
-            sha256 = "sha256-v2wctuZStb6eexcmJdkxKcGHjRk2LuZwyJvi/BerMyw=";
+            imageDigest = "sha256:4a5222f7395c659140082307bf4827edbe8bb2261b26633150e2ccbd9edf80bb";
+            sha256 = "sha256-ExAWueGDpiJMUE85H19E7Lzj3+hU9Dlyu0l0el7Tvc0=";
             tlsVerify = true;
             arch = "amd64";
             os = "linux";
